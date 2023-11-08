be8cc6bde5f57a1987be665a87c9252a8615dd6d
Neeraj Kumar <testingdach@gmail.com>
2023-11-08 14:17:32 +0530

    File Changes in Nav and Nav.js

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,42 +1,80 @@
-import React from 'react'
-import "./Nav.css" ;
-import {Link} from 'react-router-dom' ;
-import Button from '../Button/Button'
+import React, { useState, useEffect } from 'react';
+import Button from '../Button/Button';
+import { Link } from 'react-router-dom';
+import './Nav.css';
 
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-function Nav() {
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
   return (
-    <div className='nav'>
-          <div className='nav-container'>
-              <Link to="/" className='navbar-logo'>
-                Shop
-                <i class='fab fa-typo3'/>
-              </Link>
-
-              <div className='menu-item'>
-                 <ul className='menu-list'>
-                    <li className='nav-item'>
-                       <Link to="/" className='nav-links' >
-                        Home
-                       </Link>
-                    </li>
-                    <li className='nav-item'>
-                       <Link to="/service" className='nav-links' >
-                        Service
-                       </Link>
-                    </li>
-                    <li className='nav-item'>
-                       <Link to="/products" className='nav-links' >
-                        Products
-                       </Link>
-                    </li>
-                 </ul>
-              </div>
-              <Button buttonStyle='btn--outline'>SIGN UP</Button>
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            SHOP
+            <i class='fab fa-typo3' />
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
-    </div>
-    
-  )
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/services'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Nav
+export default Navbar;
